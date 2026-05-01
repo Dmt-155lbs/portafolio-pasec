565c664592041b85231346a70f12287b2ba84323
Daniel Martinez <dmartinez@estud.usfq.edu.ec>
2026-04-30 23:38:43 -0500

    Remove tailwindcss vite plugin from astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Dmt-155lbs.github.io',
   base: '/portafolio-pasec',
-  vite: {
-    plugins: [tailwindcss()]
-  },
   integrations: [react()]
 });
